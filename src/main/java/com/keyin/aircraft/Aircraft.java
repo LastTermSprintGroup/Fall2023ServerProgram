d516d2699a67eda85778e2cfadc6a83d2c8fd887
Jon Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-10-31 20:58:19 -0230

    Aircraft Service works!
    after much headache with the ids I have fixed it

--- src/main/java/com/keyin/aircraft/Aircraft.java
+++ src/main/java/com/keyin/aircraft/Aircraft.java
@@ -6,7 +6,7 @@ public class Aircraft {
 // that represent the ID of an
 // aircraft, the model of the aircraft,
 // and the amount of passengers as capacity
-    private String ID;
+    private int ID;
     private String model;
     private int capacity;
     private List<Integer> passengerIds;
@@ -14,17 +14,17 @@ public class Aircraft {
 // Basic aircraft constructor
     public Aircraft() {}
 // Aircraft constructor that assigns all values
-    public Aircraft(String ID, String model, int capacity) {
+    public Aircraft(int ID, String model, int capacity) {
         this.ID = ID;
         this.model = model;
         this.capacity = capacity;
     }
 // Getter for Aircrafts ID
-    public String getID() {
+    public int getID() {
         return ID;
     }
 // Setter for the Aircrafts ID
-    public void setID(String ID) {
+    public void setID(int ID) {
         this.ID = ID;
     }
 // Getter for the model of the Aircraft
