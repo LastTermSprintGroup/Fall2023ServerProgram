d913d846b15486d2a3be824f29e9140d982708e4
Jon Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-10-31 20:59:38 -0230

    Service handling for city and passenger
    Co-Authored-By: TylerN132 <117655789+TylerN132@users.noreply.github.com>
    Co-Authored-By: Kayleigh <84756512+kaylvigh@users.noreply.github.com>
    Co-Authored-By: TylerSGFW <117922260+TylerSGFW@users.noreply.github.com>
    Co-Authored-By: ZeroSoulz <133235219+ZeroSoulz@users.noreply.github.com>

--- src/main/java/com/keyin/city/CityController.java
+++ src/main/java/com/keyin/city/CityController.java
@@ -1,10 +1,10 @@
 package com.keyin.city;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,52 +12,42 @@ import java.util.Optional;
 @RequestMapping("/cities")
 public class CityController {
 
-    // Dummy list to mimic a database. In a real application, you would use a service and repository.
-    private List<City> cityList = new ArrayList<>();
+    @Autowired
+    private CityService cityService;
 
-    // CREATE a new city
     @PostMapping
     public ResponseEntity<City> createCity(@RequestBody City city) {
-        cityList.add(city);
-        return new ResponseEntity<>(city, HttpStatus.CREATED);
+        City createdCity = cityService.createCity(city);
+        return new ResponseEntity<>(createdCity, HttpStatus.CREATED);
     }
 
-    // READ all cities
     @GetMapping
     public ResponseEntity<List<City>> getAllCities() {
-        return new ResponseEntity<>(cityList, HttpStatus.OK);
+        List<City> cities = cityService.getAllCities();
+        return new ResponseEntity<>(cities, HttpStatus.OK);
     }
 
-    // READ a single city by ID
     @GetMapping("/{id}")
     public ResponseEntity<City> getCityById(@PathVariable int id) {
-        Optional<City> city = cityList.stream().filter(c -> c.getId() == id).findFirst();
-        if (city.isPresent()) {
-            return new ResponseEntity<>(city.get(), HttpStatus.OK);
+        Optional<City> city = cityService.getCityById(id);
+        return city.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<City> updateCity(@PathVariable int id, @RequestBody City updatedCity) {
+        City city = cityService.updateCity(id, updatedCity);
+        if (city != null) {
+            return new ResponseEntity<>(city, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    // UPDATE a city by ID
-    @PutMapping("/{id}")
-    public ResponseEntity<City> updateCity(@PathVariable int id, @RequestBody City updatedCity) {
-        for (City city : cityList) {
-            if (city.getId() == id) {
-                city.setName(updatedCity.getName());
-                city.setState(updatedCity.getState());
-                city.setPopulation(updatedCity.getPopulation());
-                return new ResponseEntity<>(city, HttpStatus.OK);
-            }
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    // DELETE a city by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCity(@PathVariable int id) {
-        if (cityList.removeIf(city -> city.getId() == id)) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // Use NO_CONTENT for successful deletes with no response body
+        boolean isDeleted = cityService.deleteCity(id);
+        if (isDeleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
