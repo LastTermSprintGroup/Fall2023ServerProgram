0ed9e0059341cc272daae750793b9c2fbded8d3e
Jon Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-11-01 18:43:51 -0230

    Update on passenger class
    Constructors were missing, needed list of aircraft and airports for passenger history

--- src/main/java/com/keyin/airport/Airport.java
+++ src/main/java/com/keyin/airport/Airport.java
@@ -4,15 +4,17 @@ public class Airport {
     private int id;
     private String name;
     private String code;
+    private int cityId;
 
 //Basic Airport Constructor
     public Airport() {}
 
 // Airport Constructor
-    public Airport(int id, String name, String code) {
+    public Airport(int id, String name, String code, int cityId) {
         this.id = id;
         this.name = name;
         this.code = code;
+        this.cityId = cityId;
     }
 // Getter for Airport ID
     public int getId() {
@@ -37,6 +39,15 @@ public class Airport {
 // Setter for Airport name
     public void setName(String name) {
         this.name = name;
+    }
+    // Getter for City ID
+    public int getCityId() {
+        return cityId;
+    }
+
+    // Setter for City ID
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
     }
 
 }
--- src/main/java/com/keyin/passenger/PassengerEntity.java
+++ src/main/java/com/keyin/passenger/PassengerEntity.java
@@ -1,15 +1,37 @@
 package com.keyin.passenger;
 
+import java.util.ArrayList;
 import java.util.List;
+
 public class PassengerEntity {
     private int passengerID;
     private String firstName;
     private String lastName;
     private int phoneNumber;
     private int cityId;
-    private List<Integer> aircraftIds;
-    //Values to store details
+    private List<Integer> aircraftIds; // List to store aircraft IDs
+    private List<Integer> airportIds; // List to store airport IDs
 
+    // Default constructor
+    public PassengerEntity() {
+        this.aircraftIds = new ArrayList<>(); // Initialize the aircraft list
+        this.airportIds = new ArrayList<>(); // Initialize the airport list
+    }
+
+    // Parameterized constructor
+    public PassengerEntity(int passengerID, String firstName, String lastName, int phoneNumber, int cityId) {
+        this.passengerID = passengerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.cityId = cityId;
+        this.aircraftIds = new ArrayList<>(); // Initialize the aircraft list
+        this.airportIds = new ArrayList<>(); // Initialize the airport list
+    }
+
+
+
+    // Getters and setters
     public int getPassengerID() {
         return passengerID;
     }
@@ -41,5 +63,49 @@ public class PassengerEntity {
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-    //Getters and setters for passenger values
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public List<Integer> getAirportIds() {
+        return airportIds;
+    }
+
+    public void setAirportIds(List<Integer> airportIds) {
+        this.airportIds = airportIds;
+    }
+
+    public List<Integer> getAircraftIds() {
+        return aircraftIds;
+    }
+
+    public void setAircraftIds(List<Integer> aircraftIds) {
+        this.aircraftIds = aircraftIds;
+    }
+
+    // Methods to manage the list of aircraft IDs
+    public void addAircraftId(int aircraftId) {
+        if (!this.aircraftIds.contains(aircraftId)) {
+            this.aircraftIds.add(aircraftId);
+        }
+    }
+
+    public void removeAircraftId(int aircraftId) {
+        this.aircraftIds.remove(Integer.valueOf(aircraftId));
+    }
+
+    public void addAirportId(int airportId) {
+        if (!this.airportIds.contains(airportId)) {
+            this.airportIds.add(airportId);
+        }
+    }
+
+    public void removeAirportId(int airportId) {
+        this.airportIds.remove(Integer.valueOf(airportId));
+    }
 }
